b5da28e7df77fbbe88ee59d6dba21f6bf836c4ae
“T-Miyabu” <takumi.miyabu@mediagene.co.jp>
2023-06-10 22:47:50 +0900

    Github Actionsによる自動デプロイ

--- codegen.ts
+++ codegen.ts
@@ -1,7 +1,7 @@
 import type { CodegenConfig } from '@graphql-codegen/cli';
 
 const config: CodegenConfig = {
-  schema: 'http://localhost:3002/api/graphql',
+  schema: 'http://localhost:3000/api/graphql',
   documents: ['./**/*.tsx'],
   generates: {
     'generated/graphql.ts': {
