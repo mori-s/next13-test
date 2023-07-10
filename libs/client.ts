cccd27d1b6e833a8f3d95d7ab41932b48ba3726c
“T-Miyabu” <takumi.miyabu@mediagene.co.jp>
2023-07-10 16:17:54 +0900

    Apolloのログ出力をより詳細にする

--- libs/client.ts
+++ libs/client.ts
@@ -1,18 +1,15 @@
 import { ApolloClient, HttpLink, InMemoryCache, from } from '@apollo/client';
 import { registerApolloClient } from '@apollo/experimental-nextjs-app-support/rsc';
-// import { headers } from 'next/dist/client/components/headers';
-// import { onError } from "@apollo/client/link/error";
+import { onError } from '@apollo/client/link/error';
 
 export const { getClient } = registerApolloClient(() => {
-  // const errorLink = onError(({ graphQLErrors, networkError }) => {
-  //   if (graphQLErrors)
-  //     graphQLErrors.forEach(({ message, locations, path }) =>
-  //       console.log(
-  //         `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
-  //       )
-  //     );
-  //   if (networkError) console.log(`[Network error]: ${networkError}`);
-  // });
+  const errorLink = onError(({ graphQLErrors, networkError }) => {
+    if (graphQLErrors)
+      graphQLErrors.forEach(({ message, locations, path }) =>
+        console.log(`[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`)
+      );
+    if (networkError) console.log(`[Network error]: ${networkError}`);
+  });
 
   const httpLink = new HttpLink({
     // https://studio.apollographql.com/public/spacex-l4uc6p/
@@ -27,6 +24,6 @@ export const { getClient } = registerApolloClient(() => {
 
   return new ApolloClient({
     cache: new InMemoryCache(),
-    link: httpLink
+    link: from([errorLink, httpLink])
   });
 });
